cbccad1a59e976996399530ae56b0cb9357f6d2f
CaretParrot <wopow29@gmail.com>
2025-07-05 11:58:35 -0500

    Add new pages and update navigation; enhance viewport settings and event handling

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-id.setupTree();
-
 let works = {
     "Nocturne": {
         originality: "original work",
@@ -28,11 +26,42 @@ let works = {
     }
 }
 
+let events = {
+}
+
 window.onload = function () {
-    for (let work in works) {
-        let newEntry = document.createElement("div");
-        newEntry.className = "work";
-        newEntry.innerHTML = `<a target="_blank" href="${works[work].link}"><h3>${work}</h3></a>`;
-        id.works.appendChild(newEntry);
+    id.setupTree();
+    if (Object.keys(works).length === 0) {
+        id.works.style.grid = "auto / 1fr";
+        id.works.innerHTML = "No works are available! Wait, how did I become a composer without writing any music? @_@";
+    } else {
+        for (let work in works) {
+            let newEntry = document.createElement("div");
+            newEntry.className = "work";
+            newEntry.innerHTML = `<a target="_blank" href="${works[work].link}"><h3>${work}</h3></a>`;
+            try {
+                id.works.appendChild(newEntry);
+            }
+            catch (error) {
+
+            }
+        }
+    }
+
+    if (Object.keys(events).length === 0) {
+        id.events.style.grid = "auto / 1fr";
+        id.events.innerHTML = "No events currently! Seems fame has slipped my grasp... :/";
+    } else {
+        for (let event in events) {
+            let newEntry = document.createElement("div");
+            newEntry.className = "event";
+            newEntry.innerHTML = `<a target="_blank" href="${events[event].link}"><h3>${event}</h3></a>`;
+            try {
+                id.events.appendChild(newEntry);
+            }
+            catch (error) {
+                console.log("No event found!");
+            }
+        }
     }
 }
