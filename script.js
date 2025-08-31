f628129dc573e36c8daf164b629cb1c9b19047e8
CaretParrot <wopow29@gmail.com>
2025-08-31 12:14:19 -0400

    Update bio and announcements sections; enhance styling for better presentation

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let announcements = [
+    {
+        date: (new Date(2025, 7, 31).toDateString()),
+        content: `<p>I've finished drafts of two works, both of which I have spent well over half a year working on: "5 Vignettes For Violin & Piano," and my first symphony. I hope to be able to make the MIDI recordings available on my <a target="_blank" href="https://youtube.com/@rvcomposition">YouTube channel</a> shortly. I'm very excited to share these new works with you.</p>
+        <p>Additionally, I have a request. If any of you would be interested in premiering any of the movements in the 5 Vignettes, please contact me! The work is scored for <strong>violin and piano</strong>, and generally demands <strong>advanced</strong> skills from both players. Key signatures and accidentals are challenging in all movements, but the work is generally tonal and requires no contemporary techniques (like Bartok pizzicato, col legno, etc). Further details are available through contact.</p>`
+    }
+]
+
 window.onload = function () {
     id.setupTree();
     id.year.innerHTML = new Date().getFullYear();
+
+    for (let i = 0; i < announcements.length; i++) {
+        let card = document.createElement("div");
+        card.innerHTML = `<h1>${announcements[i].date}</h1>${announcements[i].content}`;
+        card.classList += "card";
+        id.announcements.appendChild(card);
+    }
 }
